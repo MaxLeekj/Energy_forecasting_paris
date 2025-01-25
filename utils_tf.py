cc3d7461103fd61435823d23bcd6e83363c1d13f
Max Lee <max.lee-king-jin@student-cs.fr>
2025-01-26 02:46:43 +0800

    initial commit

---
+++ utils_tf.py
@@ -0,0 +1,335 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import IPython
+import IPython.display
+import matplotlib as mpl
+import pandas as pd
+import seaborn as sns
+
+# This file is used to configure settings use for training
+
+path = "./data/all_data.csv"
+# path = "./data/data_temp_hum_hol.csv"
+
+df = pd.read_csv(path)
+
+# df_cons = df[["y"]]
+df_cons = df[["y", "Temperature", "Humidity", "Sealevel Pressure", "Holiday"]]
+
+df_cons = df_cons.dropna()
+
+# column_indices = {name: i for i, name in enumerate(df_cons.columns)}
+
+n = len(df_cons)
+train_df = df_cons[0 : int(n * 0.7)]
+val_df = df_cons[int(n * 0.7) : int(n * 0.9)]
+test_df = df_cons[int(n * 0.9) :]
+
+# num_features = 1
+num_features = len(df_cons.columns)
+
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+
+class WindowGenerator:
+    def __init__(
+        self,
+        input_width,
+        label_width,
+        shift,
+        train_df=train_df,
+        val_df=val_df,
+        test_df=test_df,
+        label_columns=None,
+    ):
+        # Store the raw data.
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+
+        # Work out the label column indices.
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {
+                name: i for i, name in enumerate(label_columns)
+            }
+        self.column_indices = {name: i for i, name in enumerate(train_df.columns)}
+
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return "\n".join(
+            [
+                f"Total window size: {self.total_window_size}",
+                f"Input indices: {self.input_indices}",
+                f"Label indices: {self.label_indices}",
+                f"Label column name(s): {self.label_columns}",
+            ]
+        )
+
+
+def split_window(self, features):
+    inputs = features[:, self.input_slice, :]
+    labels = features[:, self.labels_slice, :]
+    if self.label_columns is not None:
+        labels = tf.stack(
+            [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+            axis=-1,
+        )
+
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+    inputs.set_shape([None, self.input_width, None])
+    labels.set_shape([None, self.label_width, None])
+
+    return inputs, labels
+
+
+WindowGenerator.split_window = split_window
+
+
+def make_dataset(self, data):
+    data = np.array(data, dtype=np.float32)
+    ds = tf.keras.utils.timeseries_dataset_from_array(
+        data=data,
+        targets=None,
+        sequence_length=self.total_window_size,
+        sequence_stride=1,
+        shuffle=True,
+        batch_size=32,
+    )
+
+    ds = ds.map(self.split_window)
+
+    return ds
+
+
+WindowGenerator.make_dataset = make_dataset
+
+MAX_EPOCHS = 20
+
+
+def compile_and_fit(model, window, patience=3):
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=patience, mode="min"
+    )
+
+    model.compile(
+        loss=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.Adam(),
+        metrics=[tf.keras.metrics.RootMeanSquaredError()],
+    )
+
+    history = model.fit(
+        window.train,
+        epochs=MAX_EPOCHS,
+        validation_data=window.val,
+        callbacks=[early_stopping],
+    )
+    return history
+
+
+@property
+def train(self):
+    return self.make_dataset(self.train_df)
+
+
+@property
+def val(self):
+    return self.make_dataset(self.val_df)
+
+
+@property
+def test(self):
+    return self.make_dataset(self.test_df)
+
+
+@property
+def example(self):
+    """Get and cache an example batch of `inputs, labels` for plotting."""
+    result = getattr(self, "_example", None)
+    if result is None:
+        # No example batch was found, so get one from the `.test` dataset
+        result = next(iter(self.test))
+        # And cache it for next time
+        self._example = result
+    return result
+
+
+WindowGenerator.train = train
+WindowGenerator.val = val
+WindowGenerator.test = test
+WindowGenerator.example = example
+
+
+def plot(self, model=None, plot_col="y", max_subplots=3):
+    inputs, labels = self.example
+    plt.figure(figsize=(12, 8))
+    plot_col_index = self.column_indices[plot_col]
+    max_n = min(max_subplots, len(inputs))
+    for n in range(max_n):
+        plt.subplot(max_n, 1, n + 1)
+        plt.ylabel(f"{plot_col} [normed]")
+        plt.plot(
+            self.input_indices,
+            inputs[n, :, plot_col_index],
+            label="Inputs",
+            marker=".",
+            zorder=-10,
+        )
+
+        if self.label_columns:
+            label_col_index = self.label_columns_indices.get(plot_col, None)
+        else:
+            label_col_index = plot_col_index
+
+        if label_col_index is None:
+            continue
+
+        plt.scatter(
+            self.label_indices,
+            labels[n, :, label_col_index],
+            edgecolors="k",
+            label="Labels",
+            c="#2ca02c",
+            s=64,
+        )
+        if model is not None:
+            predictions = model(inputs)
+            plt.scatter(
+                self.label_indices,
+                predictions[n, :, label_col_index],
+                marker="X",
+                edgecolors="k",
+                label="Predictions",
+                c="#ff7f0e",
+                s=64,
+            )
+
+        if n == 0:
+            plt.legend()
+
+    plt.xlabel("Time [h]")
+
+
+WindowGenerator.plot = plot
+
+
+def plot_error_hist(self, model=None, plot_col="y", n=1, absolute=False, bins=30):
+    inputs, labels = self.example
+    if model is not None:
+        predictions = model(inputs)
+        error = predictions - labels
+        if absolute:
+            e = abs(error[0]).numpy()
+            for i in range(1, 32):
+                e = np.append(e, abs(error[i]).numpy())
+        else:
+            e = error[0].numpy()
+            for i in range(1, 32):
+                e = np.append(e, error[i].numpy())
+        plt.hist(e, bins=bins)
+    return
+
+
+WindowGenerator.plot_error_hist = plot_error_hist
+
+
+def compare_error_hist(
+    self,
+    models=None,
+    label=None,
+    plot_col="y",
+    bins=30,
+):
+    n = 0
+    inputs, labels = self.example
+    if models is not None:
+        for model in models:
+            predictions = model(inputs)
+            error = predictions - labels
+            e = error[0].numpy()
+            for i in range(1, 32):
+                e = np.append(e, error[i].numpy())
+            plt.hist(e, alpha=0.7, bins=bins, label=label[n])
+            n += 1
+    plt.legend(loc="upper right")
+    return
+
+
+WindowGenerator.compare_error_hist = compare_error_hist
+
+
+class FeedBack(tf.keras.Model):
+    def __init__(self, units, out_steps):
+        super().__init__()
+        self.out_steps = out_steps
+        self.units = units
+        self.lstm_cell = tf.keras.layers.LSTMCell(units)
+        # Also wrap the LSTMCell in an RNN to simplify the `warmup` method.
+        self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
+        self.dense = tf.keras.layers.Dense(num_features)
+
+
+def warmup(self, inputs):
+    # inputs.shape => (batch, time, features)
+    # x.shape => (batch, lstm_units)
+    x, *state = self.lstm_rnn(inputs)
+
+    # predictions.shape => (batch, features)
+    prediction = self.dense(x)
+    return prediction, state
+
+
+FeedBack.warmup = warmup
+
+
+def call(self, inputs, training=None):
+    # Use a TensorArray to capture dynamically unrolled outputs.
+    predictions = []
+    # Initialize the LSTM state.
+    prediction, state = self.warmup(inputs)
+
+    # Insert the first prediction.
+    predictions.append(prediction)
+
+    # Run the rest of the prediction steps.
+    for n in range(1, self.out_steps):
+        # Use the last prediction as input.
+        x = prediction
+        # Execute one lstm step.
+        x, state = self.lstm_cell(x, states=state, training=training)
+        # Convert the lstm output to a prediction.
+        prediction = self.dense(x)
+        # Add the prediction to the output.
+        predictions.append(prediction)
+
+    # predictions.shape => (time, batch, features)
+    predictions = tf.stack(predictions)
+    # predictions.shape => (batch, time, features)
+    predictions = tf.transpose(predictions, [1, 0, 2])
+    return predictions
+
+
+FeedBack.call = call
